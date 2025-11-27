5d569dce3d44d4102f661061492e70503316307e
julian tulisi <julian.tulisi@elev.ga.dbgy.se>
2025-11-27 13:44:54 +0100

    uppgift1

--- 10-array/Program.cs
+++ 10-array/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] tal = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Ange heltal {i + 1}: ");
+    string? input = Console.ReadLine();
+    
+    if (int.TryParse(input, out int värde))
+    {
+        tal[i] = värde;
+    }
+    else
+    {
+        Console.WriteLine("Ogiltigt heltal. Ange ett heltal.");
+        i--;
+    }
+}
+
+Console.WriteLine("\nTalen i omvänd ordning:");
+for (int i = tal.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(tal[i]);
+}
